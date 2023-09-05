fb1c17847fa1ae19e6e3816ce769c8a32f3521aa
ondapc <admin@ondapc.com>
2023-09-05 12:10:50 +0200

    config.php

--- config.php
+++ config.php
@@ -1,11 +1,8 @@
 <?php
-// DISPLAY ERRORS / DEBUG
-ini_set('max_execution_time', 0); // NO TIME LIMIT
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-// error_reporting(0);
+error_reporting(0);
 
-// define('VERSION', time());
-define('VERSION', '1H');
+define('VERSION', '1A');
 
 require_once('functions.php');
